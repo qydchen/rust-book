03d70d696b11f3eebb03fc305cf4e9cb6b38bd28
David Chen <qydchen@gmail.com>
2024-01-16 04:45:06 -0800

    functions

---
+++ 3_common_concepts/functions/target/debug/deps/functions-abf92401c94ad020.d
@@ -0,0 +1,5 @@
+/Users/cdav/code/rust-book/3_common_concepts/functions/target/debug/deps/functions-abf92401c94ad020: src/main.rs
+
+/Users/cdav/code/rust-book/3_common_concepts/functions/target/debug/deps/functions-abf92401c94ad020.d: src/main.rs
+
+src/main.rs:
---
+++ 3_common_concepts/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/cdav/code/rust-book/3_common_concepts/functions/target/debug/functions: /Users/cdav/code/rust-book/3_common_concepts/functions/src/main.rs
